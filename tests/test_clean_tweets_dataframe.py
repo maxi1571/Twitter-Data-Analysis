39a5cb02e55c93aa9f436d22e79d7007512a4b6a
root <michaeltekle1571@gmail.com>
2021-06-24 23:11:55 +0300

    Modify test_clean_tweet_dataframe.py

--- tests/test_clean_tweets_dataframe.py
+++ tests/test_clean_tweets_dataframe.py
@@ -1,15 +1,14 @@
 import unittest
 import pandas as pd
 import sys, os
-
 from pandas.core.indexing import convert_to_index_sliceable
 sys.path.append(os.path.abspath(os.path.join('..')))
 
 from clean_tweets_dataframe import Clean_Tweets
 
-df_clean = pd.read_csv("../clean_processed_tweet_data.csv")
-
 df = pd.read_csv("../processed_tweet_data.csv")
+columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
 
 class Test_Clean_Tweet(unittest.TestCase):
 
@@ -17,24 +16,23 @@ class Test_Clean_Tweet(unittest.TestCase):
     def setUp(self) -> pd.DataFrame:
             
         self.df = Clean_Tweets(df=df)
-        self.df_clean = df_clean
         #  tweet_df = self.df.get_tweet_df()         
 
 
     def test_drop_unwanted_column(self):
-        self.assertEqual(self.df.drop_unwanted_column(df)['possibly_sensitive'].values, self.df_clean['possibly_sensitive'].values)
+        self.assertEqual(self.df.drop_unwanted_column(df).columns, columns)
 
     def test_convert_to_datetime(self):
-        self.assertEqual(self.df.convert_to_datetime(df)['creat_at'].values,self.df_clean['creat_at'].values)
+        self.assertEqual(self.df.convert_to_datetime(df)['creat_at'],[])
 
     def test_drop_duplicate(self):
-        self.assertEqual(self.df.drop_duplicate(df)['user_mentions'].values,self.df_clean['user_mentions'].values)
+        self.assertEqual(self.df.drop_duplicate(df),[])
     
     def test_convert_to_numbers(self):
-        self.assertEqual(self.df.convert_to_numbers(df)['polarity'].values,self.df_clean['polarity'].values) 
+        self.assertEqual(type(self.df.convert_to_numbers(df)['polarity'][0]),int) 
 
     def test_remove_non_english_tweets(self):
-        self.assertEqual(self.df.remove_non_english_tweets(df)['original_text'].values,self.df_clean['original_text'].values)
+        self.assertEqual(self.df.remove_non_english_tweets(df)['lang'][50],'eng')
 
 if __name__ == '__main__':
 	unittest.main()
